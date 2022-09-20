55deb8bca666d038e52ef805457c837c4133c58b
AkshayDube <dubeyakshay09@gmail.com>
2022-09-20 12:15:16 +0530

    Provide support for metrics,health,info,threaddump,env,mappings,beans using actuator and configuration in application.properties file

---
+++ actuator/src/main/java/com/assignment/actuator/Repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.assignment.actuator.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.assignment.actuator.Entities.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>{
+
+}
