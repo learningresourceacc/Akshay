9694a4d07ed650a2f81b8c16b4eaad6a6ca0b5d1
AkshayDube <dubeyakshay09@gmail.com>
2022-09-21 15:58:20 +0530

    Added User entity and added details required to do database related work automatically in application.properties file

---
+++ jpaSecurity/src/main/java/com/spring/jpaSecurity/Repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.spring.jpaSecurity.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.jpaSecurity.Entities.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>{
+}
