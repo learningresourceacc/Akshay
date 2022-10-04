2d74afd14badffc0c190049befbdaa63ca91716d
AkshayDube <dubeyakshay09@gmail.com>
2022-10-04 15:00:52 +0530

    Inital Commit for Jpa Bidirectional Project

---
+++ JpaBidirectional/src/main/java/com/example/demo/JpaBidirectionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBidirectionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBidirectionalApplication.class, args);
+	}
+
+}
